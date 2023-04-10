4e524f5c0cc1844b538acedf03e2891ea03ee413
devshinon-br <rezendekeyla126@gmail.com>
2023-04-10 13:00:00 -0300

    Adicionando configurações iniciais

--- src/main/java/mvc/sql/proficiencytest/ProficiencyTestApplication.java
+++ src/main/java/mvc/sql/proficiencytest/ProficiencyTestApplication.java
@@ -1,13 +1,28 @@
 package mvc.sql.proficiencytest;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ProficiencyTestApplication {
+	private static final String DB_URL  = "jdbc:postgresql://localhost:5432/estacionamento";
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProficiencyTestApplication.class, args);
+	}
+
+	@Bean(initMethod = "migrate")
+	public Flyway flyway(final Environment environment) {
+		return Flyway.configure()
+				.dataSource(
+						environment.getProperty("spring.datasource.url"),
+						environment.getProperty("spring.datasource.username"),
+						environment.getProperty("spring.datasource.password"))
+				.locations(environment.getProperty("spring.flyway.locations"))
+				.load();
 	}
 
 }
